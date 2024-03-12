4dda4320963b567bf2bae6e12a6839fb87fe7bc0
cristian-bazan <cristianbazan.cr@gmail.com>
2024-03-12 18:27:56 -0300

    Agregar archivos de proyecto.

---
+++ Gestor_de_Remitos/Servicios/DB_Helper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Gestor_de_Remitos.Servicios
+{
+    public class DB_Helper
+    {
+        string connectionString = GetConnectionString("config.json");
+
+        private static string GetConnectionString(string configFile)
+        {
+            string json = File.ReadAllText(configFile);
+            dynamic config = JsonConvert.DeserializeObject(json);
+            string connectionString = config.ConnectionString;
+
+            return connectionString;
+        }
+        public DB_Helper()
+        {
+                
+        }
+
+        public string algo()
+        {
+            return connectionString;
+        }
+    }
+
+}
